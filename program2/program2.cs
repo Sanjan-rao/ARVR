f60e2dcd3c205065f761cb628919c9981d579f22
MayurGP25 <114596885+MayurGP25@users.noreply.github.com>
2025-02-21 22:11:50 +0530

    Update program2.cs

--- program2/program2.cs
+++ program2/program2.cs
@@ -1,44 +1,38 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChangeAppearance : MonoBehaviour
+public class Program2 : MonoBehaviour
 {
-    public GameObject cube, sphere, plane;  // Assign objects in the Inspector
-    public Material newCubeMaterial, newSphereMaterial, newPlaneMaterial;  // New materials
-    public Texture newCubeTexture, newSphereTexture, newPlaneTexture;  // New textures
+    public GameObject cube;
+    public GameObject sphere;
+    public GameObject plane;
+    public Material newMaterial;
+    public Texture newTexture;
 
-    // Function to change Cube appearance
-    public void ChangeCubeAppearance()
-    {
-        Renderer cubeRenderer = cube.GetComponent<Renderer>();
-        cubeRenderer.material = newCubeMaterial;  // Change Material
-        cubeRenderer.material.mainTexture = newCubeTexture;  // Change Texture
-        cubeRenderer.material.color = Color.red;  // Change Color
-    }
-
-    // Function to change Sphere appearance
-    public void ChangeSphereAppearance()
-    {
-        Renderer sphereRenderer = sphere.GetComponent<Renderer>();
-        sphereRenderer.material = newSphereMaterial;
-        sphereRenderer.material.mainTexture = newSphereTexture;
-        sphereRenderer.material.color = Color.green;
-    }
-
-    // Function to change Plane appearance
-    public void ChangePlaneAppearance()
-    {
-        Renderer planeRenderer = plane.GetComponent<Renderer>();
-        planeRenderer.material = newPlaneMaterial;
-        planeRenderer.material.mainTexture = newPlaneTexture;
-        planeRenderer.material.color = Color.blue;
-    }
+    public Button cubeButton;
+    public Button sphereButton;
+    public Button planeButton;
 
     void Start()
-{
-    ChangeCubeAppearance();
-    ChangeSphereAppearance();
-    ChangePlaneAppearance();
-}
+    {
+        cubeButton.onClick.AddListener(ChangeCubeMaterial);
+        sphereButton.onClick.AddListener(ChangeSphereTexture);
+        planeButton.onClick.AddListener(ChangePlaneMaterial);
+    }
 
+    void ChangeCubeMaterial()
+    {
+        cube.GetComponent<Renderer>().material = newMaterial;
+    }
+
+    void ChangeSphereTexture()
+    {
+        sphere.GetComponent<Renderer>().material.mainTexture = newTexture;
+    }
+
+    void ChangePlaneMaterial()
+    {
+        plane.GetComponent<Renderer>().material = newMaterial;
+        plane.GetComponent<Renderer>().material.mainTexture = newTexture;
+    }
 }
